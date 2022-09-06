05e9a6661392f3ee31947b31e76b6b3b4f0fd11d
jeseduardopi <jeseduardopi@gmail.com>
2022-09-06 13:37:51 +0200

    Change testing container

---
+++ src/components/SwitchTest.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Switch from "./Switch.svelte";
+  
+      const handleToggle = (value) => {
+        console.log(value)
+      }
+
+  </script>
+  
+  <main>
+    <Switch label="Apples or pinapples?" onToggle={handleToggle} />
+  </main>
