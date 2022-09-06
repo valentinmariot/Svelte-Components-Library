5dcb2aa30c011dabbd72a4fc11c9ff9b71bc710f
jeseduardopi <jeseduardopi@gmail.com>
2022-09-06 11:26:47 +0200

    Add switch component

---
+++ src/components/Switch.svelte
@@ -0,0 +1,38 @@
+<script>
+    let count = 0
+    const increment = () => {
+      count += 1
+    }
+  </script>
+  
+  <button>
+    <div class="button-switch">
+    </div>
+  </button>
+  
+  <style>
+    button {
+        flex-shrink: 0;
+        height: 24px;
+        width: 50px;
+        /* background-color: ${({ $isToggled, theme }) => ($isToggled ? theme.colors.primary2 : theme.colors.neutral3)}; */
+        border-radius: 20px;
+        cursor: pointer;
+        display: flex;
+        position: relative;
+        transition: background-color 150ms ease-in-out;
+        padding: 0 0;
+    }
+    
+    .button-switch {
+        height: 20px;
+        width: 20px;
+        margin: 1px;
+        background-color: white;
+        border-radius: inherit;
+        position: relative;
+        transition: transform 150ms ease-in-out;
+        z-index: 1;
+    }
+
+  </style>
