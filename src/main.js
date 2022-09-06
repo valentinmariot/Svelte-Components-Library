7fe00486c611ba76ac3ac67a51799f9a7a33e640
jeseduardopi <jeseduardopi@gmail.com>
2022-09-06 17:00:07 +0200

    Add format style props

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './app.css'
-import App from './App.svelte'
+import SwitchTest from './components/Switch/SwitchTest.svelte'
 
-const app = new App({
+const app = new SwitchTest({
   target: document.getElementById('app')
 })
 
