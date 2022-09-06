6085c60d2a6b75511077b7fa4f8b955bbf2cec16
jeseduardopi <jeseduardopi@gmail.com>
2022-09-06 18:29:54 +0200

    Add menu nav

--- src/components/Menu/Menu.svelte
+++ src/components/Menu/Menu.svelte
@@ -1,11 +1,77 @@
 <script>
-    let count = 0
-    const increment = () => {
-      count += 1
-    }
-  </script>
-  
-  <button on:click={increment}>
-    count is {count}
-  </button>
-  
+  import { fly } from 'svelte/transition'
+
+  import AnimatedHamburger from './BurgerMenu.svelte'
+  // import logo from './svelte-logo.svg'
+
+  export let open = false
+  export let onClick = () => {
+    open = !open
+  }
+
+  let items = ['qsd','qsdfqdsq', 'qsdfqsd']
+</script>
+
+<header>
+  <div class="main">
+    <div class="logo">
+    
+    </div>
+    <AnimatedHamburger {open} {onClick} />
+  </div>
+
+  {#if open}
+    <nav>
+      {#each items as item}
+      <!-- <a href="">Videos</a>
+      <a href="">Blogposts</a>
+      <a href="">Guides</a>
+      <a href="">Portfolio</a>
+      <a href="">Milestones</a> -->
+        <a  >{item}</a>
+      {/each}
+    </nav>
+  {/if}
+</header>
+
+<style>
+  header {
+    position: relative;
+    font-size: 2rem;
+    z-index: 2;
+  }
+
+  .main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  a {
+    width: 100%;
+   cursor: pointer; 
+  }
+  a:hover{
+    background: pink;
+  }
+  a:active{
+
+  }
+  a:focus {
+
+  }
+  a:link{
+    text-decoration: none;
+  }
+
+
+  nav {
+    text-align: center;
+    background-color: grey;
+    position: absolute;
+    z-index: -1;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    align-items: center;
+  }
+</style>
