30eb1d34c9f258e9397b945dba5437ff460a471f
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2022-09-07 10:50:12 +0200

    First commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,32 @@
 <script>
-</script>
-
-<class class="ico-apple"></class>
-
-<style>
-</style>
+    import Modal from "./Modal.svelte";
+    let modal;
+    let current = 'sup';
+  </script>
+  <button on:click={() => modal.show()}>Deleted count</button>
+  
+  <Modal bind:this={modal}>
+    <h2>Modal title</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation
+    </p>
+    <button on:click={() => modal.hide()}>Cancel</button>
+    <button class="{current === 'sup' ? 'selected' : ''}" on:click="{() => modal.hide()}"> Yes, deleted it </button>
+  
+  </Modal>
+  
+  <style>
+    button {
+      position: block;
+      border: 2px solid rgba(0, 24, 88, 0.5);
+      border-radius: 3px;
+      float:right;
+      }
+      .selected {
+      background: rgba(245, 130, 174, 0.7);
+        color: #D04077
+      }
+  </style>
+  
+  
---
+++ src/Modal.svelte
@@ -0,0 +1,59 @@
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+
+<script>
+    let open = false;
+    export function show() {
+        open = true;
+    }
+    export function hide() {
+        open = false;
+    }
+    
+</script>
+
+{#if open}
+    <div class="pop-up">
+        <div class="modal">
+            <span class="close" on:click={() => hide()}>&times;</span>
+            <slot />
+        </div>
+    </div>
+{/if}
+<style>
+    .pop-up {
+        /* background-color: rgb(0, 0, 0);
+        background-color: rgba(0, 0, 0, 0.6); */
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+    }
+
+    .modal {
+        background-color: rgba(245, 130, 174, 0.3);
+        color: black;
+        
+        max-width: 80vw;
+        padding: 1rem;
+        margin: 15% auto;
+        border: 3px solid #001858;
+        border-radius: 3px;
+
+        width: 350px;
+        height: 190px;
+        font-family: 'Roboto', sans-serif;
+        font-size: 14px;
+        /* left: 120px;
+        top: 125px; */
+    }
+
+    .close {
+        float: right;
+        cursor: pointer;
+    }
+
+    .close:hover {
+        font-weight: bold;
+    }
+</style>
