b609f1642b208986c736e21dda8d2c685456ef26
jeseduardopi <jeseduardopi@gmail.com>
2022-09-07 11:18:45 +0200

    Add section with different component states

--- src/components/Switch/SwitchTest.svelte
+++ src/components/Switch/SwitchTest.svelte
@@ -7,6 +7,35 @@
 
   </script>
   
-  <main>
-    <Switch label="Apples or pinapples?" onToggle={handleToggle} />
-  </main>
+    <section>
+      <h1>Switch</h1>
+      <hr>
+      <br>
+    <div class='container'>
+      <div class='center'>
+      <Switch  label={'Ok or not ok?'} onToggle={handleToggle} />
+    </div>
+    
+      <br>
+
+      <div class='center'>
+        <Switch  label={'Locked'} isLocked />
+      </div>
+
+      <br>
+
+      <div class="center">
+        <Switch  label={'Disabled'} isDisabled />
+      </div>
+      <br>
+      </div>
+
+    </section>
+
+  <style>
+    .center{
+      display: flex;
+      justify-content: right;
+
+    }
+  </style>
