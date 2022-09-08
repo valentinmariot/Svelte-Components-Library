37ca4405ed3d190458acfcd0a141206bb06500f2
Ada Loveless <xhorj@protonmail.com>
2022-09-08 10:08:48 +0200

    feat: added radio buttons to select color

--- src/components/Loader/Loader.svelte
+++ src/components/Loader/Loader.svelte
@@ -7,15 +7,17 @@
 
 {#if isLoading}
 <div class="loader">
-<Omelette color={selectedColor}/>
-<label>
-	<input type=radio bind:group={selectedColor} name="pink" value={"pink"}>
-Theme 1
-</label>
-<label>
-	<input type=radio bind:group={selectedColor} name="blue" value={"blue"}>
-Theme 2
-</label>
+    <Omelette color={selectedColor}/>
+    <div class="color-radios">
+        <label class="color-radios__label">
+            <input class="color-radios__input" type=radio bind:group={selectedColor} name="pink" value={"pink"}>
+        Theme 1
+        </label>
+        <label class="color-radios__label">
+            <input class="color-radios__input" type=radio bind:group={selectedColor} name="blue" value={"blue"}>
+        Theme 2
+        </label>
+    </div>
 </div>
 {/if}
 
@@ -24,9 +26,20 @@ Theme 2
     width: 100%;
     height: 100%;
     display:flex;
-    position: relative;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     }
+
+    .color-radios {
+        padding-top : 20px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .color-radios__label {
+        padding-top: 10px;
+    }
+
 
 </style>
