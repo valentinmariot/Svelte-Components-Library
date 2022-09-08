1167a5885cfbdc676c517b49ed14a030efa08dff
jeseduardopi <jeseduardopi@gmail.com>
2022-09-08 09:59:40 +0200

    Fix style

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import './app.css'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import SwitchTest from './components/Switch/SwitchTest.svelte'
-
-const app = new SwitchTest({
-=======
-import Menu from './components/Menu/MenuTest.svelte'
-
-const app = new Menu({
->>>>>>> 0f81643 (Add isLeft prop)
-=======
 import App from './App.svelte'
 
 const app = new App({
->>>>>>> 1d6cb22 (Update app)
   target: document.getElementById('app')
 })
 
