bf90aafc3373d34832a01f3009e0c39d5f71e06a
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2022-09-08 09:55:55 +0200

    Added font

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,4 +1,4 @@
-<!-- <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'> -->
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 
 <script>
     let open = false;
