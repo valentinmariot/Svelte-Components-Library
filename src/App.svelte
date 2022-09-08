f278d80e3ffb3acb0200c698b5dd67060a712641
Ada Loveless <xhorj@protonmail.com>
2022-09-08 09:52:03 +0200

    feat: creating loader

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
+    import Loader from "./components/Loader/Loader.svelte";
 </script>
 
 <class class="ico-apple"></class>
+<Loader isLoading={true}/>
 
 <style>
 </style>
---
+++ src/components/Loader/Loader.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Omelette from "../Omelette/Omelette.svelte";
+    export let isLoading // <Boolean> 
+    let selectedColor = 'pink' // <String> "pink" | "blue"
+    
+</script>
+
+{#if isLoading}
+<div class="loader">
+<Omelette color={selectedColor}/>
+<label>
+	<input type=radio bind:group={selectedColor} name="pink" value={"pink"}>
+Theme 1
+</label>
+<label>
+	<input type=radio bind:group={selectedColor} name="blue" value={"blue"}>
+Theme 2
+</label>
+</div>
+{/if}
+
+<style>
+    .loader{
+    width: 100%;
+    height: 100%;
+    display:flex;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+    }
+
+</style>
